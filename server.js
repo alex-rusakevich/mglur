2756d11cbf8cfcd28dc9474a5796cd8cc0be017b
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2024-11-04 19:39:43 +0300

    #patch https redirect if production mode

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = 3000
 
 require('dotenv').config()
 
+console.log(`Starting the server in ${process.env.NODE_ENV} mode`)
 
 const router = express.Router()
 router.use('/static', express.static(__dirname + '/static'))
@@ -11,8 +12,16 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.use((req, res, next) => {
+    if (!req.secure && process.env.NODE_ENV == "production") {
+        res.redirect('https://' + req.headers.host + req.url)
+    } else {
+        next()
+    }
+})
+
 app.use(process.env.URL_PREFIX || "", router);
 
 app.listen(port, () => {
-    console.log(`Mglur app listening on port ${port}`)
+    console.log(`Mglur app listening on address http://127.0.0.1:${port}`)
 })
